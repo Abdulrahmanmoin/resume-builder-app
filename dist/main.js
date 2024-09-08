926f12f0be79885a9977202f0f102c1d130e46a8
Abdulrahmanmoin <iam.armoin@gmail.com>
2024-09-08 19:51:16 +0500

    Add generate button and edit button.

--- dist/main.js
+++ dist/main.js
@@ -1,11 +1,37 @@
 "use strict";
 // Object.defineProperty(exports, "__esModule", { value: true });
-let resumeSec = document.querySelector(".resume");
-let headerBtn = document.querySelector(".header-btn");
-let form = document.querySelector(".form");
+const resumeSec = document.querySelector(".resume");
+const headerBtn = document.querySelector(".header-btn");
+const form = document.querySelector(".form");
+const nameField = document.querySelector(".name-field");
+const phoneNumField = document.querySelector(".phone-num-field");
+const educationField = document.querySelector(".education-field");
+const skillsField = document.querySelector(".skills-field");
+const designationField = document.querySelector(".designation-field");
+const experienceField = document.querySelector(".experience-field");
+const nameResume = document.querySelector(".name-resume");
+const designationResume = document.querySelector(".designation-resume");
+const phoneNumResume = document.querySelector(".phone-num-resume");
+const skillResume = document.querySelector(".skill-resume");
+const eduResume = document.querySelector(".edu-resume");
+const experienceResume = document.querySelector(".experience-resume");
+const generateBtn = document.querySelector(".generate-btn");
 form.style.display = "none";
-headerBtn.addEventListener("click", () => {
+headerBtn.addEventListener("click", (event) => {
     resumeSec.style.display = "none";
     form.style.display = "block";
-    // console.log("hell0");
+    headerBtn.style.display = "none";
+});
+generateBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    form.style.display = "none";
+    resumeSec.style.display = "block";
+    headerBtn.style.display = "block";
+    headerBtn.textContent = "Edit your resume";
+    nameResume.textContent = nameField.value;
+    phoneNumResume.textContent = phoneNumField.value;
+    designationResume.textContent = designationField.value;
+    skillResume.textContent = skillsField.value;
+    eduResume.textContent = educationField.value;
+    experienceResume.textContent = experienceField.value;
 });
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,51 @@
-let resumeSec = document.querySelector(".resume") as HTMLOptionElement
-let headerBtn = document.querySelector(".header-btn") as HTMLButtonElement
-let form = document.querySelector(".form") as HTMLFormElement
+const resumeSec = document.querySelector(".resume") as HTMLOptionElement
+const headerBtn = document.querySelector(".header-btn") as HTMLButtonElement
+const form = document.querySelector(".form") as HTMLFormElement
+
+const nameField = document.querySelector(".name-field") as HTMLInputElement
+const phoneNumField = document.querySelector(".phone-num-field") as HTMLInputElement
+const educationField = document.querySelector(".education-field") as HTMLInputElement
+const skillsField = document.querySelector(".skills-field") as HTMLInputElement
+const designationField = document.querySelector(".designation-field") as HTMLInputElement
+const experienceField = document.querySelector(".experience-field") as HTMLInputElement
+
+const nameResume = document.querySelector(".name-resume") as HTMLInputElement
+const designationResume = document.querySelector(".designation-resume") as HTMLInputElement
+const phoneNumResume = document.querySelector(".phone-num-resume") as HTMLInputElement
+const skillResume = document.querySelector(".skill-resume") as HTMLInputElement
+const eduResume = document.querySelector(".edu-resume") as HTMLInputElement
+const experienceResume = document.querySelector(".experience-resume") as HTMLInputElement
+
+const generateBtn = document.querySelector(".generate-btn") as HTMLInputElement
 
 form.style.display = "none"
 
-headerBtn.addEventListener("click", () => {
+headerBtn.addEventListener("click", (event: MouseEvent): void => {
     resumeSec.style.display = "none"
     form.style.display = "block"
+    headerBtn.style.display = "none";
 
-    // console.log("hell0");
+
 
 })
+
+
+generateBtn.addEventListener("click", (e: MouseEvent) : void => {
+
+    e.preventDefault()
+
+    form.style.display = "none"
+    resumeSec.style.display = "block"
+
+    headerBtn.style.display = "block";
+    headerBtn.textContent = "Edit your resume";
+
+
+    nameResume.textContent = nameField.value;
+    phoneNumResume.textContent = phoneNumField.value;
+    designationResume.textContent = designationField.value;
+    skillResume.textContent = skillsField.value;
+    eduResume.textContent = educationField.value;
+    experienceResume.textContent = experienceField.value;
+})
+
