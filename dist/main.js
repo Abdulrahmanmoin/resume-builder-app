56ea1d6218609d220098c92099cddb737cffce12
Abdulrahmanmoin <iam.armoin@gmail.com>
2024-09-08 20:36:17 +0500

    Add form validation

--- dist/main.js
+++ dist/main.js
@@ -2,13 +2,14 @@
 // Object.defineProperty(exports, "__esModule", { value: true });
 const resumeSec = document.querySelector(".resume");
 const headerBtn = document.querySelector(".header-btn");
-const form = document.querySelector(".form");
+const form = document.querySelector(".form form");
 const nameField = document.querySelector(".name-field");
 const phoneNumField = document.querySelector(".phone-num-field");
 const educationField = document.querySelector(".education-field");
 const skillsField = document.querySelector(".skills-field");
 const designationField = document.querySelector(".designation-field");
 const experienceField = document.querySelector(".experience-field");
+const genResumeHeading = document.querySelector(".gen-resume-heading");
 const nameResume = document.querySelector(".name-resume");
 const designationResume = document.querySelector(".designation-resume");
 const phoneNumResume = document.querySelector(".phone-num-resume");
@@ -17,14 +18,21 @@ const eduResume = document.querySelector(".edu-resume");
 const experienceResume = document.querySelector(".experience-resume");
 const generateBtn = document.querySelector(".generate-btn");
 form.style.display = "none";
+genResumeHeading.style.display = "none";
 headerBtn.addEventListener("click", (event) => {
     resumeSec.style.display = "none";
     form.style.display = "block";
+    genResumeHeading.style.display = "block";
     headerBtn.style.display = "none";
 });
 generateBtn.addEventListener("click", (e) => {
+    if (!form.checkValidity()) {
+        form.reportValidity();
+        return;
+    }
     e.preventDefault();
     form.style.display = "none";
+    genResumeHeading.style.display = "none";
     resumeSec.style.display = "block";
     headerBtn.style.display = "block";
     headerBtn.textContent = "Edit your resume";
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-const resumeSec = document.querySelector(".resume") as HTMLOptionElement
+const resumeSec = document.querySelector(".resume") as HTMLElement
 const headerBtn = document.querySelector(".header-btn") as HTMLButtonElement
-const form = document.querySelector(".form") as HTMLFormElement
+const form = document.querySelector(".form form") as HTMLFormElement
 
 const nameField = document.querySelector(".name-field") as HTMLInputElement
 const phoneNumField = document.querySelector(".phone-num-field") as HTMLInputElement
@@ -9,20 +9,26 @@ const skillsField = document.querySelector(".skills-field") as HTMLInputElement
 const designationField = document.querySelector(".designation-field") as HTMLInputElement
 const experienceField = document.querySelector(".experience-field") as HTMLInputElement
 
-const nameResume = document.querySelector(".name-resume") as HTMLInputElement
-const designationResume = document.querySelector(".designation-resume") as HTMLInputElement
-const phoneNumResume = document.querySelector(".phone-num-resume") as HTMLInputElement
-const skillResume = document.querySelector(".skill-resume") as HTMLInputElement
-const eduResume = document.querySelector(".edu-resume") as HTMLInputElement
-const experienceResume = document.querySelector(".experience-resume") as HTMLInputElement
+const genResumeHeading = document.querySelector(".gen-resume-heading") as HTMLHeadingElement;
 
-const generateBtn = document.querySelector(".generate-btn") as HTMLInputElement
+const nameResume = document.querySelector(".name-resume") as HTMLHeadingElement
+const designationResume = document.querySelector(".designation-resume") as HTMLParagraphElement
+const phoneNumResume = document.querySelector(".phone-num-resume") as HTMLParagraphElement
+const skillResume = document.querySelector(".skill-resume") as HTMLParagraphElement
+const eduResume = document.querySelector(".edu-resume") as HTMLParagraphElement
+const experienceResume = document.querySelector(".experience-resume") as HTMLParagraphElement
+
+const generateBtn = document.querySelector(".generate-btn") as HTMLButtonElement
 
 form.style.display = "none"
+genResumeHeading.style.display = "none"
 
 headerBtn.addEventListener("click", (event: MouseEvent): void => {
     resumeSec.style.display = "none"
     form.style.display = "block"
+    genResumeHeading.style.display = "block"
+
+
     headerBtn.style.display = "none";
 
 
@@ -30,11 +36,19 @@ headerBtn.addEventListener("click", (event: MouseEvent): void => {
 })
 
 
-generateBtn.addEventListener("click", (e: MouseEvent) : void => {
+generateBtn.addEventListener("click", (e: MouseEvent): void => {
+
+    if (!form.checkValidity()) {
+        
+        form.reportValidity();
+        return
+    }
 
     e.preventDefault()
 
     form.style.display = "none"
+    genResumeHeading.style.display = "none"
+
     resumeSec.style.display = "block"
 
     headerBtn.style.display = "block";
